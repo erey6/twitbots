93f559136277bd3da0fef676f9211bbcbacccc2d
erey6 <42914433+erey6@users.noreply.github.com>
2020-03-03 13:00:55 -0600

    fixed all messages to hawk, all Sox

--- hawkbdays.py
+++ hawkbdays.py
@@ -3,12 +3,16 @@ from __future__ import print_function
 import mlbgame
 import random
 import tweepy
-import twitter_credentials
+import hk_twitter_credentials
 from datetime import datetime
 
-auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+auth = tweepy.OAuthHandler(hk_twitter_credentials.CONSUMER_KEY, hk_twitter_credentials.CONSUMER_SECRET)
+auth.set_access_token(hk_twitter_credentials.ACCESS_TOKEN, hk_twitter_credentials.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
+
+currentDay = datetime.now().day
+currentMonth = datetime.now().month
+currentYear = datetime.now().year
 
 classics = [["Carlton", "Fisk", "1947-12-26"],
             ["Harold", "Baines", "1959-03-15"],
@@ -24,6 +28,7 @@ classics = [["Carlton", "Fisk", "1947-12-26"],
 rip_classics = [["Nellie", "Fox","1927-12-25"],
                 ["Minnie", "Minoso", "1925-11-29"],
                 ["Luke", "Appling", "1907-04-02"]]
+
 fans = [['Barack', 'Obama', '1961-08-04'],
         ['Dennis', 'DeYoung', '1947-02-18'],
         ['Steve ', 'Dahl', '1954-11-20'],
@@ -48,22 +53,20 @@ for person in fans:
     z = dict(zip(["name_first", "name_last", "birth_date"], person))
     fans_dicts.append(z)
 
-currentDay = datetime.now().day
-currentMonth = datetime.now().month
-currentYear = datetime.now().year
-cubs_roster = mlbgame.roster(112)
+
+sox_roster = mlbgame.roster(145)
 
 
 
-for i in cubs_roster.players:
+for i in sox_roster.players:
     dob = i.birth_date
     b_year = dob[0:4]
     b_month = dob[5:7]
     b_date = dob[8:10]
     age = currentYear - int(b_year)
     if int(b_date) == currentDay and int(b_month) == currentMonth:
-        closing = random.choice(["I'll see you on Rush Street.", "This Bud's for you!"])
-        my_status = "{} {} turns {} today. Happy birthday, {}! {}\n\n#Cubs".format(i.name_first, i.name_last, age, i.name_first, closing)
+        closing = random.choice(["D.J. and I wish you the best.", "Yesssssss!"])
+        my_status = "{} {} turns {} today. Happy birthday, {}! {}\n\n#WhiteSox".format(i.name_first, i.name_last, age, i.name_first, closing)
         api.update_status(status=my_status)
 
 
@@ -74,8 +77,8 @@ for item in classic_dicts:
     b_date = dob[8:10]
     age = currentYear - int(b_year)
     if int(b_date) == currentDay and int(b_month) == currentMonth:
-        closing = random.choice(["I'll see you on Rush Street.", "This Bud's for you!"])
-        my_status = "{} {} turns {} today. Happy birthday, {}! {}\n\n#Cubs @Cubs".format(item["name_first"], item["name_last"], age, item["name_first"], closing)
+        closing = random.choice(["D.J. and I wish you the best.", "Yesssssss!"])
+        my_status = "{} {} turns {} today. Happy birthday, {}! {}\n\n#WhiteSox".format(item["name_first"], item["name_last"], age, item["name_first"], closing)
         #print(my_status)
         api.update_status(status=my_status)
 
@@ -86,8 +89,8 @@ for item in fans_dicts:
     b_date = dob[8:10]
     age = currentYear - int(b_year)
     if int(b_date) == currentDay and int(b_month) == currentMonth:
-        closing = random.choice(["I'll see you on Rush Street.", "This Bud's for you!"])
-        my_status = "Longtime Cubs fan {} {} turns {} today. Happy birthday, {}! {}\n\n#Cubs @Cubs".format(item["name_first"], item["name_last"], age, item["name_first"], closing)
+        closing = random.choice(["D.J. and I wish you the best.", "Yesssssss!"])
+        my_status = "Longtime Sox fan {} {} turns {} today. Happy birthday, {}! {}\n\n#WhiteSox".format(item["name_first"], item["name_last"], age, item["name_first"], closing)
         #print(my_status)
         api.update_status(status=my_status)
 
@@ -98,8 +101,8 @@ for item in rip_classic_dicts:
     b_date = dob[8:10]
     age = currentYear - int(b_year)
     if int(b_date) == currentDay and int(b_month) == currentMonth:
-        closing = random.choice(["Everybody raise a can of ice cold Bud Light.", "This Bud's for you! "])
-        my_status = "{} {} was born {} years ago today. Happy birthday, {}! Gone but not forgotten. {}\n\n#Cubs @Cubs ".format(item["name_first"], item["name_last"], age, item["name_first"], closing)
+        closing = random.choice(["One of the greats!", "Real White Sox fans remember the great ones! "])
+        my_status = "{} {} was born {} years ago on this date. Let's remember, {}! Gone but not forgotten. {}\n\n#WhiteSox".format(item["name_first"], item["name_last"], age, item["name_first"], closing)
         #print(my_status)
         api.update_status(status=my_status)
 
